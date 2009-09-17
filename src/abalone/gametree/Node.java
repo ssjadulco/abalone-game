74da2fbe38420eac0c9e1e05783ff0160e6a4132
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-09-17 13:24:50 +0000

    added comments to methods. Still needs lots of work.

--- src/abalone/gametree/Node.java
+++ src/abalone/gametree/Node.java
@@ -1,6 +1,6 @@
 /*
- * Node class.
- * Nodes are positions in a game.
+ * Node class
+ * Nodes are positions in a game
  */
 
 package abalone.gametree;
@@ -9,33 +9,53 @@ import java.util.ArrayList;
 
 public class Node {
 
-    // Need arraylist for node, because can have more childeren than two.
+    // Need arraylist for node, because can have more childeren than two
     private ArrayList<Node> children;
 
-    // Need some data in nodes. Currently int, will change to object (gamestate?).
+    // Need some data in nodes. Currently int, will change to object (gamestate?)
     private int data;
 
-    // Constructor for node class.
+    // Constructor for node class
     public Node() {
         children = new ArrayList<Node>();
     }
 
-    // Method to add children.
+   /**
+    * Method to add a child to a specific node
+    * @param n node n
+    */
     public void addChild(Node n) {
         children.add(n);
     }
 
-    // Method to get children..
+   /**
+    * Method to return one child from a specific node.
+    * @return  one child from a specified node
+    */
+    public void getChild(){
+        // remove void, add return, some code, etc.
+    }
+
+   /**
+    * Method to return all children from a specific node
+    * @return  all children from a specified node
+    */
     public ArrayList<Node> getChildren() {
     return children;
     }
 
-    // Method to set data into a node.
+   /**
+    * Method which sets data in some node
+    * @param newdata is new data which needs to be put into a node
+    */
     public void setData(int newdata) {
         data = newdata;
     }
 
-    // Method to get data from a node.
+   /**
+    * Method to return data from a specific node
+    * @return  the data in a node
+    */
     public int getData() {
         return data;
     }
