3cdbcbd79df2139a4266eef9d0f36ce93d874899
ale.wrk <ale.wrk@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-09-24 15:33:46 +0000

    updated the game tree data structure

--- trunk/src/abalone/gametree/Tree.java
+++ trunk/src/abalone/gametree/Tree.java
@@ -1,10 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package abalone.gametree;
+
+import java.util.ArrayList;
 
 public class Tree {
 
+	private Node root;
+	
+	public Node getRoot()
+	{
+		return root;
+	}
+	
+	public void setRoot(Node root)
+	{
+		this.root = root;
+	}
+	
+	public Node getParent(Node n)
+	{
+		return n.getParent();
+	}
+	
+	public ArrayList<Node> getChildren(Node n)
+	{
+		return n.getChildren();
+	}
 }
