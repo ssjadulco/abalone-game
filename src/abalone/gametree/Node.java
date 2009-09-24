c1c3bff758b83b6323ef64cc3cdd71238f7e3ab0
ale.wrk <ale.wrk@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-09-24 15:33:46 +0000

    updated the game tree data structure

--- src/abalone/gametree/Node.java
+++ src/abalone/gametree/Node.java
@@ -1,62 +1,48 @@
-/*
- * Node class
- * Nodes are positions in a game
- */
-
 package abalone.gametree;
 
 import java.util.ArrayList;
+import abalone.gamestate.GameState;
 
 public class Node {
 
-    // Need arraylist for node, because can have more childeren than two
+	private Node parent;
     private ArrayList<Node> children;
-
-    // Need some data in nodes. Currently int, will change to object (gamestate?)
-    private int data;
-
-    // Constructor for node class
+    private GameState data;
+    
     public Node() {
         children = new ArrayList<Node>();
     }
+    public void setParent(Node parent)
+    {
+    	this.parent = parent;
+    }
+    
+    public Node getParent()
+    {
+    	return this.parent;
+    }
 
-   /**
-    * Method to add a child to a specific node
-    * @param n node n
-    */
     public void addChild(Node n) {
         children.add(n);
     }
 
-   /**
-    * Method to return one child from a specific node.
-    * @return  one child from a specified node
-    */
-    public void getChild(){
-        // remove void, add return, some code, etc.
+    public GameState getChild(int index){
+    	return (GameState) children.get(index).getData();
     }
 
-   /**
-    * Method to return all children from a specific node
-    * @return  all children from a specified node
-    */
     public ArrayList<Node> getChildren() {
     return children;
     }
+    
+    public void removeChild(int index){
+    	children.remove(index);
+    }
 
-   /**
-    * Method which sets data in some node
-    * @param newdata is new data which needs to be put into a node
-    */
-    public void setData(int newdata) {
+    public void setData(GameState newdata) {
         data = newdata;
     }
 
-   /**
-    * Method to return data from a specific node
-    * @return  the data in a node
-    */
-    public int getData() {
+    public GameState getData() {
         return data;
     }
 }
