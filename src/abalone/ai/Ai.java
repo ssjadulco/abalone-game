28c2f715494a6ad1e35a43ea15298ed8c195cf83
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-09-29 13:35:32 +0000

    Small fixes, nothing serious.

--- src/abalone/ai/Ai.java
+++ src/abalone/ai/Ai.java
@@ -5,8 +5,18 @@
 
 package abalone.ai;
 
+import java.util.List;
+
+import abalone.gamestate.GameState;
+import abalone.gametree.Node;
+import abalone.gametree.Tree;
+import abalone.gamelogic.GameLogic;
+
+
 public interface Ai {
 
     // TODO Add some generic methods for the algorithms.
+    // obtain gamestate
+    // etc, etc, etc
 
 }
