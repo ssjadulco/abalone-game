4580e2ca704089257d6eecc6bcc13f35b47f1492
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-10-01 12:23:53 +0000

    Updated interface.

---
+++ src/abalone/ai/Algorithms.java
@@ -0,0 +1,16 @@
+/*
+ * Interface class for the search Algorithms.
+ * 
+ */
+
+package abalone.ai;
+
+// TODO Need to do some imports here. Will do as we progress.
+
+public interface Algorithms extends Ai {
+
+    // TODO Add some search algorithm methods here.
+    // initialize somethings, run search algorithms, etc
+    // etc, etc, etc
+
+}
