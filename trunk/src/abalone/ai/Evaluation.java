b655ec00575cbaecd887d516abdf8eed31aba96f
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-10-01 12:23:53 +0000

    Updated interface.

---
+++ trunk/src/abalone/ai/Evaluation.java
@@ -0,0 +1,17 @@
+/*
+ * Interface class for the Evaluation functions.
+ * 
+ */
+
+package abalone.ai;
+
+// TODO Need to do some imports here. Will do as we progress.
+
+public interface Evaluation extends Ai {
+
+    // TODO Add some evaluation methods here.
+    // initialize somethings, run evaluation algorithms, etc
+    // etc, etc, etc
+
+
+}
