27cdfab3dc93a3af7cb114ecad241ddde344be97
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-10-01 12:26:02 +0000

    Updated interface.

--- trunk/src/abalone/ai/Ai.java
+++ trunk/src/abalone/ai/Ai.java
@@ -8,19 +8,15 @@ package abalone.ai;
 import java.util.List;
 // Some imports here, probably won't use most, will change later.
 import abalone.gamestate.GameState;
-import abalone.gametree.Node;
-import abalone.gametree.Tree;
-import abalone.gamelogic.GameLogic;
+import abalone.model.Player;
 
 
-public interface Ai {
+public interface Ai extends Player {
 
     // TODO Add some generic methods for using AI in general.
     // initialize somethings, run AI, etc
     // etc, etc, etc
 
     public void readGamestate(GameState state);
-    
-
-
+   
 }
