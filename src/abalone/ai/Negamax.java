3a505431cf9bf978278137f541236a2691fcba4d
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-10-01 12:23:41 +0000

    Updated interface.

---
+++ src/abalone/ai/Negamax.java
@@ -0,0 +1,32 @@
+/*
+ * Negamax is minimax variant for two player zero-sun game.
+ * Adding alpha-beta pruning to it. Currently in pseudocode state.
+ */
+
+package abalone.ai;
+
+import abalone.gamestate.GameState;
+import abalone.gametree.Node;
+import abalone.gametree.Tree;
+
+public class Negamax implements Algorithms {    
+
+    public void readGamestate(GameState state) {
+      // TODO read gamestate here.
+    }
+
+    public void doNegamax(Node n, int depth, double alpha, double beta){
+        if(n == rootOfTree || depth == 0 ){
+            return evaluationValueOfNode;
+        }
+        else
+            // For each child of node
+            for(getChildren : node){
+                alpha = max(alpha, -negamax(child, depth-1, -beta, -alpha,));
+                if (alpha >= beta){
+                    return alpha;
+                }
+            }
+            return alpha;
+    }
+}
