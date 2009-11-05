a1ead0e5ba00d760e71554b55e48a0866a4956b0
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-11-05 11:06:00 +0000

    Small javadocs updated.

--- src/abalone/ai/LinearEvaluator.java
+++ src/abalone/ai/LinearEvaluator.java
@@ -1,24 +1,24 @@
 /*
- *  Class file for our linear evaluation function, for determining how good a move is, without going through the whole gametree.
+ *  A Linear Evaluation function based on a paper by 
  */
 
 package abalone.ai;
 
+// Imports from Java libraries.
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
-        
+// Imports from other libraries.
 import search.tree.heuristic.Evaluator;
 import search.tree.SearchState;
 import abalone.gamestate.GameState;
 import abalone.model.Player;
 
-
 public class LinearEvaluator implements Evaluator
 {
 
-    // variables for strategies.
+    // Variables for strategies.
     private double f1;
     private double f2;
     private double f3;
@@ -26,7 +26,7 @@ public class LinearEvaluator implements Evaluator
     private double f5;
     private double f6;
 
-    // variables for weights of each strategy.
+    // Variables for weights of each strategy.
     private double w1;
     private double w2;
     private double w3;
@@ -34,8 +34,7 @@ public class LinearEvaluator implements Evaluator
     private double w5;
     private double w6;
 
-    // variable for evaluation function.
-
+    // Constructor for LinearEvaluator class.
     public LinearEvaluator(){
         w1 = 1;
         w2 = 1;
@@ -45,8 +44,13 @@ public class LinearEvaluator implements Evaluator
         w6 = 1;
     }
 
+   /**
+    * Method which evaluates the 'goodness' of a move done in a search state.
+    * @param    state   a searchstate which the AI wishes to evaluate
+    * @return           the 'goodness' of a searched move
+    */
     public eval(SearchState state){
-        // checks to see if it is okay.
+        // Checks to see if the searchstate given is an instance of a gamestate.
         if(state instanceof GameState){
 
             GameState s = (GameState) state;
@@ -62,20 +66,32 @@ public class LinearEvaluator implements Evaluator
         }
     }
 
-    // Calculates the difference between sum of Manhattan distances of each player's marbles.
-    private double calculateF1(List<Player>){
-        for()
+   /**
+    * Method which calculates the difference between the sum of the Manhattan distances of marbles for each players.
+    * @param    Player  a list of players
+    * @param
+    * @return
+    */
+    private double calculateF1(){
 
         return f1;
     }
     
-    //
+   /**
+    * Returns the 
+    * @param
+    * @return      
+    */
     private double calculateF2(){
 
         return f2;
     }
 
-    //
+   /**
+    * Returns the
+    * @param
+    * @return
+    */
     private double calculateF3(){
 
         return f3;
@@ -100,50 +116,98 @@ public class LinearEvaluator implements Evaluator
 
     }
 
+   /**
+    * Getter method which gives weight of function 1 (w1).
+    * @return           the weight of function 1 (w1)
+    */
     public double getW1(){
         return w1;
     }
 
+   /**
+    * Setter method which changes weight of function 1 (w1).
+    * @param    w1  the new weight of function 1 (w1)
+    */
     public void setW1(double w1) {
         this.w1 = w1;
     }
 
+   /**
+    * Getter method which gives weight of function 2 (w2).
+    * @return           the weight of function 2 (w2)
+    */
     public double getW2() {
         return w2;
     }
 
+   /**
+    * Setter method which changes weight of function 2 (w2).
+    * @param    w2  the new weight of function 2 (w2)
+    */
     public void setW2(double w2){
         this.w2 = w2;
     }
 
+   /**
+    * Getter method which gives weight of function 3 (w3).
+    * @return           the weight of function 3 (w3)
+    */
     public double getW3(){
         return w3;
     }
 
+   /**
+    * Setter method which changes weight of function 3 (w3).
+    * @param    w3  the new weight of function 3 (w3)
+    */
     public void setW3(double w3){
         this.w3 = w3;
     }
 
+   /**
+    * Getter method which gives weight of function 4 (w4).
+    * @return           the weight of function 4 (w4)
+    */
     public double getW4(){
         return w4;
     }
 
+   /**
+    * Setter method which changes weight of function 4 (w4).
+    * @param    w4  the new weight of function 4 (w4)
+    */
     public void setW4(double w4){
         this.w4 = w4;
     }
 
+   /**
+    * Getter method which gives weight of function 5 (w5).
+    * @return           the weight of function 5 (w5)
+    */
     public double getW5(){
         return w5;
     }
 
+   /**
+    * Setter method which changes weight of function 5 (w5).
+    * @param    w4  the new weight of function 5 (w5)
+    */
     public void setW5(double w5){
         this.w5 = w5;
     }
 
+   /**
+    * Getter method which gives weight of function 6 (w6).
+    * @return           the weight of function 6 (w6)
+    */
     public double getW6(){
         return w6;
     }
 
+   /**
+    * Setter method which changes weight of function 6 (w6).
+    * @param    w4  the new weight of function 6 (w6)
+    */
     public void setW6(double w6){
         this.w6 = w6;
     }
