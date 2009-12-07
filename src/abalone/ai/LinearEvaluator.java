37c423bc6cc02f872fe85431360a8935b79de705
re.vandriel@student.maastrichtuniversity.nl <re.vandriel@student.maastrichtuniversity.nl@b6af269c-98ef-11de-979f-3176e26b92dd>
2009-12-07 10:23:27 +0000

    This is now the weighted linear evaluator without weight training. OptimizedLinearEvaluator is used by the genetic algorithmn.

--- src/abalone/ai/LinearEvaluator.java
+++ src/abalone/ai/LinearEvaluator.java
@@ -1,9 +1,10 @@
 /*
  *  A linear, weighted evaluation function based on a paper 'Constructing an Abalone Game-Playing Agent'
  *  by N.P.P.M. Lemmens (18th June 2005).
- * 
+ *
+ *  The weights are NOT trained in this version.
+ *
  */
-
 package abalone.ai;
 
 // Imports from Java libraries.
@@ -22,18 +23,19 @@ import search.tree.SearchState;
 // Imports from Gamestate library.
 import abalone.gamestate.GameState;
 
+public class LinearEvaluator implements Evaluator<Double> {
 
-public class LinearEvaluator implements Evaluator<Double>
-{
+    // Variable for initial game state.
+    private GameState initialState;
 
-    // Variables for strategies.
+    // Variables for the individual functions.
     private double f1;
     private double f2;
     private double f3;
     private double f4;
     private double f5;
     private double f6;
-
+    
     // Variables for weights of each strategy.
     private double w1;
     private double w2;
@@ -43,7 +45,11 @@ public class LinearEvaluator implements Evaluator<Double>
     private double w6;
 
     // Constructor for LinearEvaluator class.
-    public LinearEvaluator(GameState state){
+    public LinearEvaluator(GameState initialState) {
+
+        // Set the initialState.
+        this.initialState = initialState;
+
         // Ensure functions are set back to default values.
         f1 = 0;
         f2 = 0;
@@ -51,284 +57,213 @@ public class LinearEvaluator implements Evaluator<Double>
         f4 = 0;
         f5 = 0;
         f6 = 0;
+
         // Ensure weights are set back to default values.
-        w1 = 1;
-        w2 = 1;
-        w3 = 1;
-        w4 = 1;
+        w1 = .05;
+        w2 = .01;
+        w3 = .01;
+        w4 = .01;
         w5 = 1;
         w6 = 1;
     }
 
-   /**
-    * Method which evaluates the desirability of a move done in a search state.
-    * @param    state   a searchstate which the AI wishes to evaluate
-    * @return           the desirability of a searched move
-    */
-    public Double eval(SearchState state){
+    /**
+     * Method which evaluates the desirability of a move done in a search state.
+     *
+     * @param state
+     *            a searchstate which the AI wishes to evaluate
+     * @return the desirability of a searched move
+     */
+    @Override
+    public Double eval(SearchState state) {
+
+        double eval = 0;
+
         // Checks to see if the searchstate given is an instance of a gamestate.
-        if(state instanceof GameState){
+        if (state instanceof GameState) {
             // Cast searchstate to gamestate.
             GameState s = (GameState) state;
             // Get the current player.
-            Player currentPlayer = s.getCurrentPlayer();
+            Player currentPlayer = initialState.getCurrentPlayer();
             // Get the opponent player.
-            //int tempCur = s.getPlayers().indexOf(s.getCurrentPlayer());
-            //Player opponentPlayer = s.getPlayers().get((tempCur + 1) % 2);
-            Player opponentPlayer = s.getOpponentPlayer();
+            Player opponentPlayer = initialState.getOpponentPlayer();
             // Get current player's marbles.
             Set<Node> currentPlayerMarbles = s.getMarbles(currentPlayer);
             // Get opponent player's marbles.
             Set<Node> opponentPlayerMarbles = s.getMarbles(opponentPlayer);
             // Get the lost marbles per player.
             Map<Player, Integer> lostMarbles = s.getMarblesRemoved();
-         
-            // Calculate the individual functions.
-            calculateF1(currentPlayerMarbles, opponentPlayerMarbles);
-            calculateF2(currentPlayerMarbles, opponentPlayerMarbles, s);
-            calculateF3(currentPlayerMarbles, opponentPlayerMarbles, s);
-            calculateF4(currentPlayerMarbles, opponentPlayerMarbles, s);
-            calculateF5(lostMarbles, opponentPlayer);
-            calculateF6(lostMarbles, currentPlayer);
 
-            // Evaluate.
-            double eval = (w1 * f1) + (w2 * f2) + (w3 * f3) + (w4 * f4) + (w5 * f5) - (w6 * f6);
+            // Calculations for current player.
+            int currentPlayerManhattanDistanceCount = 0;
+            int currentPlayerTeammatesCount = 0;
+            int currentPlayerBreakStrongGroupStrategyCount = 0;
+            int currentPlayerStrengthenGroupStrategyCount = 0;
+            for (Node node : currentPlayerMarbles) {
+                currentPlayerManhattanDistanceCount += node.getManhDist();
+                for (Direction d : Direction.UPPER_LEFT) {
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == s.getMarbleOwner(node)) {
+                        currentPlayerTeammatesCount++;
+                    }
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == opponentPlayer && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == opponentPlayer) {
+                        currentPlayerBreakStrongGroupStrategyCount++;
+                    }
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == opponentPlayer && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == currentPlayer) {
+                        currentPlayerStrengthenGroupStrategyCount++;
+                    }
+                }
+            }
+
+            // Calculations for opponent player.
+            int opponentPlayerManhattanDistanceCount = 0;
+            int opponentPlayerTeammatesCount = 0;
+            int opponentPlayerBreakStrongGroupStrategyCount = 0;
+            int opponentPlayerStrengthenGroupStrategyCount = 0;
+            for (Node node : opponentPlayerMarbles) {
+                opponentPlayerManhattanDistanceCount += node.getManhDist();
+                for (Direction d : Direction.UPPER_LEFT) {
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == s.getMarbleOwner(node)) {
+                        opponentPlayerTeammatesCount++;
+                    }
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == currentPlayer && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == currentPlayer) {
+                        opponentPlayerBreakStrongGroupStrategyCount++;
+                    }
+                    if (s.getMarbleOwner(node.getNeighbour(d)) == currentPlayer && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == opponentPlayer) {
+                        opponentPlayerStrengthenGroupStrategyCount++;
+                    }
+                }
+            }
+
+            // Calculation of individual functions.
+            f1 = opponentPlayerManhattanDistanceCount - currentPlayerManhattanDistanceCount;
+            f2 = currentPlayerTeammatesCount - opponentPlayerTeammatesCount;
+            f3 = currentPlayerBreakStrongGroupStrategyCount - opponentPlayerBreakStrongGroupStrategyCount;
+            f4 = currentPlayerStrengthenGroupStrategyCount - opponentPlayerStrengthenGroupStrategyCount;
+            f5 = lostMarbles.get(opponentPlayer);
+            f6 = lostMarbles.get(currentPlayer);
+
+            // Evaluation.
+            eval = (w1 * f1) + (w2 * f2) + (w3 * f3) + (w4 * f4) + (w5 * f5) - (w6 * f6);
+
+            // Scale between -1 and 1 (-1 being a lost game and 1 being a win game).
+
             return eval;
+        } else {
+            return 0.0;
         }
-        else{
-
-        }       
-        return 0d;
     }
 
-   /**
-    * Method which calculates the difference between the sum of the Manhattan distances of marbles for each players.
-    * <p>
-    * This compares how close two players marbles are to the center. Reason for this is that in Abalone being at the
-    * center is a good strategy, as it makes it harder for a player's marbles to be knocked off the board.
-    * @param
-    * @param
-    * @return
-    */
-    private double calculateF1(Set<Node> currentPlayerMarbles, Set<Node> opponentPlayerMarbles){
-        int currentCount = 0;
-        int opponentCount = 0;
-        for (Node node : currentPlayerMarbles) {
-            currentCount += node.getManhDist();
-        }
-        for (Node node : opponentPlayerMarbles) {
-            opponentCount += node.getManhDist();
-        }
-        f1 = opponentCount - currentCount;
-        return f1;
-    }
-    
-   /**
-    * Method which calculates the difference between the sum of neighboring teammates of each marble for each player.
-    * <p>
-    * This is also known as the Cohesion Strategy. Reason for this is that in Abalone it is good for a player to have
-    * his/her marbles close to each other, as it adds offensive and defensive capabilities.
-    * @param
-    * @param
-    * @return      
-    */
-    private double calculateF2(Set<Node> currentPlayerMarbles, Set<Node> opponentPlayerMarbles, GameState s){
-        int currentCount = 0;
-        int opponentCount = 0; 
-        for (Node node : currentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getMarbleOwner(node)){
-                    currentCount++;
-                }
-            }
-        }
-        for (Node node : opponentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getMarbleOwner(node)){
-                    opponentCount++;
-                }
-            }
-        }
-        f2 = currentCount - opponentCount;
-        return f2;
-    }
-
-   /**
-    * Method which calculates the difference between the sum of opponent marble on adjacent side and opponent marble on
-    * opposing adjacent side.
-    * <p>
-    * This is also known as the break-strong-group strategy. Reason is that when you break a group of the opponent, it
-    * will weaken them offensively and defensively. Also often, when inbetween two opponent groups, a player's marbles
-    * cannot be pushed.
-    * @param
-    * @param
-    * @return
-    */
-    private double calculateF3(Set<Node> currentPlayerMarbles, Set<Node> opponentPlayerMarbles, GameState s){
-        int currentCount = 0;
-        int opponentCount = 0;
-        for (Node node : currentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getOpponentPlayer() && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == s.getOpponentPlayer()){
-                    currentCount++;
-                }
-            }
-        }
-        for (Node node : opponentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getCurrentPlayer() && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == s.getCurrentPlayer()){
-                    opponentCount++;
-                }
-            }
-        }
-        f3 = currentCount - opponentCount;
-        return f3;
-    }
-
-   /**
-    * Method which calculates
-    * <p>
-    * This is also known as the strengthen-group strategy. Reason is if you have contact position with an opponent marble,
-    * it should be reinforced to ensure that it is safe from attack, but also to add offensive capabilities.
-    * @param
-    * @param
-    * @return
-    */
-    private double calculateF4(Set<Node> currentPlayerMarbles, Set<Node> opponentPlayerMarbles, GameState s){
-        int currentCount = 0;
-        int opponentCount = 0;
-        for (Node node : currentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getOpponentPlayer() && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == s.getCurrentPlayer()){
-                    currentCount++;
-                }
-            }
-        }
-        for (Node node : opponentPlayerMarbles) {
-            for (Direction d : Direction.UPPER_LEFT) {
-                if(s.getMarbleOwner(node.getNeighbour(d)) == s.getCurrentPlayer() && s.getMarbleOwner(node.getNeighbour(d.getOpposite())) == s.getOpponentPlayer()){
-                    opponentCount++;
-                }
-            }
-        }
-        f4 = currentCount - opponentCount;
-        return f4;
-    }
-
-   /**
-    * Method which calculates the number of marbles the opponent has lost in this searchstate.
-    * @param    Map a map containing the
-    * @param    Player a opponent player
-    * @return   
-    */
-    private double calculateF5(Map<Player, Integer> removed, Player opponentPlayer ){
-        f5 = removed.get(opponentPlayer);
-        return f5;
-    }
-
-   /**
-    * Method which calculates the number of marbles the player has lost in this searchstate.
-    * @param    Player  a list of players
-    * @return
-    */
-    private double calculateF6(Map<Player, Integer> removed, Player currentPlayer){
-        f6 = removed.get(currentPlayer);
-        return f6;
-    }
-
-   /**
-    * Getter method which gives weight of function 1 (w1).
-    * @return           the weight of function 1 (w1)
-    */
-    public double getW1(){
+    /**
+     * Getter method which gives weight of function 1 (w1).
+     *
+     * @return the weight of function 1 (w1)
+     */
+    public double getW1() {
         return w1;
     }
 
-   /**
-    * Setter method which changes weight of function 1 (w1).
-    * @param    w1  the new weight of function 1 (w1)
-    */
+    /**
+     * Setter method which changes weight of function 1 (w1).
+     *
+     * @param w1
+     *            the new weight of function 1 (w1)
+     */
     public void setW1(double w1) {
         this.w1 = w1;
     }
 
-   /**
-    * Getter method which gives weight of function 2 (w2).
-    * @return           the weight of function 2 (w2)
-    */
+    /**
+     * Getter method which gives weight of function 2 (w2).
+     *
+     * @return the weight of function 2 (w2)
+     */
     public double getW2() {
         return w2;
     }
 
-   /**
-    * Setter method which changes weight of function 2 (w2).
-    * @param    w2  the new weight of function 2 (w2)
-    */
-    public void setW2(double w2){
+    /**
+     * Setter method which changes weight of function 2 (w2).
+     *
+     * @param w2
+     *            the new weight of function 2 (w2)
+     */
+    public void setW2(double w2) {
         this.w2 = w2;
     }
 
-   /**
-    * Getter method which gives weight of function 3 (w3).
-    * @return           the weight of function 3 (w3)
-    */
-    public double getW3(){
+    /**
+     * Getter method which gives weight of function 3 (w3).
+     *
+     * @return the weight of function 3 (w3)
+     */
+    public double getW3() {
         return w3;
     }
 
-   /**
-    * Setter method which changes weight of function 3 (w3).
-    * @param    w3  the new weight of function 3 (w3)
-    */
-    public void setW3(double w3){
+    /**
+     * Setter method which changes weight of function 3 (w3).
+     *
+     * @param w3
+     *            the new weight of function 3 (w3)
+     */
+    public void setW3(double w3) {
         this.w3 = w3;
     }
 
-   /**
-    * Getter method which gives weight of function 4 (w4).
-    * @return           the weight of function 4 (w4)
-    */
-    public double getW4(){
+    /**
+     * Getter method which gives weight of function 4 (w4).
+     *
+     * @return the weight of function 4 (w4)
+     */
+    public double getW4() {
         return w4;
     }
 
-   /**
-    * Setter method which changes weight of function 4 (w4).
-    * @param    w4  the new weight of function 4 (w4)
-    */
-    public void setW4(double w4){
+    /**
+     * Setter method which changes weight of function 4 (w4).
+     *
+     * @param w4
+     *            the new weight of function 4 (w4)
+     */
+    public void setW4(double w4) {
         this.w4 = w4;
     }
 
-   /**
-    * Getter method which gives weight of function 5 (w5).
-    * @return           the weight of function 5 (w5)
-    */
-    public double getW5(){
+    /**
+     * Getter method which gives weight of function 5 (w5).
+     *
+     * @return the weight of function 5 (w5)
+     */
+    public double getW5() {
         return w5;
     }
 
-   /**
-    * Setter method which changes weight of function 5 (w5).
-    * @param    w5  the new weight of function 5 (w5)
-    */
-    public void setW5(double w5){
+    /**
+     * Setter method which changes weight of function 5 (w5).
+     *
+     * @param w5
+     *            the new weight of function 5 (w5)
+     */
+    public void setW5(double w5) {
         this.w5 = w5;
     }
 
-   /**
-    * Getter method which gives weight of function 6 (w6).
-    * @return           the weight of function 6 (w6)
-    */
-    public double getW6(){
+    /**
+     * Getter method which gives weight of function 6 (w6).
+     *
+     * @return the weight of function 6 (w6)
+     */
+    public double getW6() {
         return w6;
     }
 
-   /**
-    * Setter method which changes weight of function 6 (w6).
-    * @param    w6  the new weight of function 6 (w6)
-    */
-    public void setW6(double w6){
+    /**
+     * Setter method which changes weight of function 6 (w6).
+     *
+     * @param w6
+     *            the new weight of function 6 (w6)
+     */
+    public void setW6(double w6) {
         this.w6 = w6;
     }
 }
